86a0fc738b16fe54c8154067ec4423d61f6dd1be
Вадя <vadim.filatov.02@gmail.com>
2024-09-18 01:54:46 +0700

    Сделал дз

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,21 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        String firstName = "Ivan ";
+        String middleName = " Ivanov ";
+        String lastName = "Ivanovich";
+        String fullName = middleName + firstName + lastName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+        System.out.println("Задание 2");
 
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+        System.out.println("Задание 3");
+
+        String middleNameTwo = "Иванов ";
+        String firstNameTwo = "Семён ";
+        String lastNameTwo = "Семёнович";
+        fullName = middleNameTwo + firstNameTwo.replace("ё", "е")
+                + lastNameTwo.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName);
     }
 }
